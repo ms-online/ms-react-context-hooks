e784618ab9982f46fd1a09f824c3f290fdacbf9d
ms-online <27732357@qq.com>
2020-11-04 11:36:06 +0800

    lesson-11

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4} from 'uuid'
 
 const SongList = () => {
+
+   const [songs, setSongs] = useState([
+        { title: 'Lemon', id: 1 },
+        { title: '打上花火', id: 2 },
+        { title: 'Loser', id: 3 },
+   ])
+    
+    const addSong = () => {
+        setSongs([...songs,{title: '春雷', id:uuidv4()}])
+    }
     return ( 
         <div className="song-list">
             <ul>
-                <li>Lemon - 米津玄师</li>
-                <li>打上花火 - 米津玄师</li>
-                <li>Loser- 米津玄师</li>
+                {songs.map(song => {
+                    return (
+                        <li key = { song.id}>{song.title}</li>
+                    )
+                })}
             </ul>
+            <button onClick = {addSong}>添加歌曲</button>
         </div>
      );
 }
