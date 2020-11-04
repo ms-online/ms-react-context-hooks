a5272000f6fade426ec04fa29580ceb4f69bf593
ms-online <27732357@qq.com>
2020-11-04 19:51:28 +0800

    lesson-14

--- contextapp/src/conponent/BookList.js
+++ contextapp/src/conponent/BookList.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext'
 
-class BookList extends Component {
-    static contextType = ThemeContext
-    render() { 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return ( 
-            <div className="book-list" style={{color:theme.syntax, background:theme.bg}}>
-                <ul>
-                    <li style = {{background:theme.ui}}>撒哈拉的故事</li>
-                    <li style = {{background:theme.ui}}>梦里花落知多少</li>
-                    <li style = {{background:theme.ui}}>雨季不再来</li>
-                </ul>
-            </div>
-         );
-    }
-}
- 
+// class BookList extends Component {
+//     static contextType = ThemeContext
+//     render() { 
+//         const { isLightTheme, light, dark } = this.context;
+//         const theme = isLightTheme ? light : dark;
+//         return ( 
+//             <div className="book-list" style={{color:theme.syntax, background:theme.bg}}>
+//                 <ul>
+//                     <li style = {{background:theme.ui}}>撒哈拉的故事</li>
+//                     <li style = {{background:theme.ui}}>梦里花落知多少</li>
+//                     <li style = {{background:theme.ui}}>雨季不再来</li>
+//                 </ul>
+//             </div>
+//          );
+//     }
+// }
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return ( 
+        <div className="book-list" style={{color:theme.syntax, background:theme.bg}}>
+        <ul>
+            <li style = {{background:theme.ui}}>撒哈拉的故事</li>
+            <li style = {{background:theme.ui}}>梦里花落知多少</li>
+            <li style = {{background:theme.ui}}>雨季不再来</li>
+        </ul>
+    </div>
+    );
+  }
+   
 export default BookList;
+ 
