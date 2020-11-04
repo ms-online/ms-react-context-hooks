98e337f39a9352acf8668529dcfc5d68889fbb50
ms-online <27732357@qq.com>
2020-11-04 20:19:24 +0800

    lesson-16

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -2,14 +2,17 @@ import BookList from './conponent/BookList'
 import Navbar from './conponent/Navbar'
 import ThemeToggle from './conponent/ThemeToggle'
 import AuthContextProvider from './contexts/AuthContext'
+import BookContextProvider from './contexts/BookContext'
 import ThemeContextProvider from './contexts/ThemeContext'
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <AuthContextProvider>
-        <Navbar />
-        <BookList />
+          <Navbar />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
         <ThemeToggle />
         </AuthContextProvider>
       </ThemeContextProvider>
